2ea1860adb9af8e1ea3272eeddb57ee18b08df47
Pablo Andres Vaquiro Suarez <vaquiro1974@gmail.com>
2025-09-22 22:30:41 -0500

    fix: correct ESLint errors and configure React version

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,9 +1,9 @@
 /*global cy*/
 
 describe('Pokedex' , function() {
-    it('frontpage can be opened', function() {
-        cy.visit('/');
-        cy.contains('pikachu')
-        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
+  it('frontpage can be opened', function() {
+    cy.visit('/')
+    cy.contains('pikachu')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 })
