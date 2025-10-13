f3f667c66fd1d3471566b3e181c659695706f8c4
Pablo Andres Vaquiro Suarez <vaquiro1974@gmail.com>
2025-10-12 20:31:52 -0500

    fix: correct console log for Heroku and healthcheck

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/version', (req, res) => {
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port ${PORT}') // eslint-disable-line no-console
+  console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
